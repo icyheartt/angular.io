1b4a65efe91b7f951bcb6498f51a1e17ee1728ac
icyheartt <qorcksrb9131@gmail.com>
2024-11-27 13:56:46 +0900

    wishlist

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,16 +1,35 @@
-
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
   styleUrls: ['./wishlist.component.css']
 })
-export class WishlistComponent {
-  @Output() movieSelected = new EventEmitter<string>();
+export class WishlistComponent implements OnInit {
+  movies: any[] = [];
+  isTableView = true; // true: Table View, false: Infinite Scroll
 
-  selectMovie(movie: string) {
-    this.movieSelected.emit(movie);
+  ngOnInit(): void {
+    this.loadWishlist();
   }
-  constructor() { }
+
+  // Local Storage에서 찜한 영화 데이터 로드
+  loadWishlist(): void {
+    const storedMovies = localStorage.getItem('wishlist');
+    this.movies = storedMovies ? JSON.parse(storedMovies) : [];
+  }
+
+  // 찜한 영화 삭제
+  removeFromWishlist(movieId: number): void {
+    const storedMovies = localStorage.getItem('wishlist');
+    let wishlist = storedMovies ? JSON.parse(storedMovies) : [];
+    wishlist = wishlist.filter((movie: any) => movie.id !== movieId);
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+    this.movies = wishlist;
+  }
+
+  // View 전환
+  toggleView(): void {
+    this.isTableView = !this.isTableView;
+  }
 }
